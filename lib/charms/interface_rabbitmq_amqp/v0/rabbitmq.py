9bfca63104c72d41c452958e9706a500e25a393d
David Ames <david.ames@canonical.com>
2021-05-19 08:41:24 -0700

    INIT

---
+++ lib/charms/interface_rabbitmq_amqp/v0/rabbitmq.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""RabbitMQAMQPProvides and Requires module"""
+
+import logging
+
+from ops.framework import (
+    StoredState,
+    EventBase,
+    ObjectEvents,
+    EventSource,
+    Object)
+
+logger = logging.getLogger(__name__)
+
+
+class HasAMQPServersEvent(EventBase):
+    """Has AMQPServers Event."""
+    pass
+
+
+class ReadyAMQPServersEvent(EventBase):
+    pass
+
+
+class RabbitMQAMQPServerEvents(ObjectEvents):
+    has_amqp_servers = EventSource(HasAMQPServersEvent)
+    ready_amqp_servers = EventSource(ReadyAMQPServersEvent)
+
+
+class RabbitMQAMQPRequires(Object):
+    """
+    RabbitMQAMQPRequires class
+    """
+
+    on = RabbitMQAMQPServerEvents()
+    _stored = StoredState()
+
+    def __init__(self, charm, relation_name):
+        super().__init__(charm, relation_name)
+        self.charm = charm
+        self.relation_name = relation_name
+        self.framework.observe(
+            self.charm.on[relation_name].relation_joined, self._on_amqp_relation_joined
+        )
+        self.framework.observe(
+            self.charm.on[relation_name].relation_changed, self._on_amqp_relation_changed
+        )
+        self.framework.observe(
+            self.charm.on[relation_name].relation_broken, self._on_amqp_relation_broken
+        )
+
+    @property
+    def _amqp_rel(self):
+        return self.framework.model.get_relation(self.relation_name)
+
+    def _on_amqp_relation_joined(self, event):
+        logging.debug("RabbitMQAMQPRequires on_joined")
+        self.on.has_amqp_servers.emit()
+
+    def _on_amqp_relation_changed(self, event):
+        logging.debug("RabbitMQAMQPRequires on_changed")
+        # Validate data on the relation
+        if self.username(event) and self.vhost(event):
+            self.on.ready_amqp_servers.emit()
+
+    def _on_amqp_relation_broken(self, event):
+        logging.debug("RabbitMQAMQPRequires on_departed")
+        # TODO clear data on the relation
+
+    def username(self, event):
+        return event.relation.data[self.charm.app].get("username")
+
+    def vhost(self, event):
+        return event.relation.data[self.charm.app].get("vhost")
+
+    def password(self, event):
+        return event.relation.data[self.charm.app].get("password")
+
+    def request_access(self, event, username, vhost):
+        logging.debug("Requesting AMQP user and vhost")
+        event.relation.data[self.charm.app]["username"] = username
+        event.relation.data[self.charm.app]["vhost"] = (
+            self.vhost(event))
+
+
+class HasAMQPClientsEvent(EventBase):
+    """Has AMQPClients Event."""
+    pass
+
+
+class ReadyAMQPClientsEvent(EventBase):
+    pass
+
+
+class RabbitMQAMQPClientEvents(ObjectEvents):
+    has_amqp_clients = EventSource(HasAMQPClientsEvent)
+    ready_amqp_clients = EventSource(ReadyAMQPClientsEvent)
+
+
+class RabbitMQAMQPProvides(Object):
+    """
+    RabbitMQAMQPProvides class
+    """
+
+    on = RabbitMQAMQPClientEvents()
+    _stored = StoredState()
+
+    def __init__(self, charm, relation_name):
+        super().__init__(charm, relation_name)
+        self.charm = charm
+        self.relation_name = relation_name
+        self.framework.observe(
+            self.charm.on[relation_name].relation_joined, self._on_amqp_relation_joined
+        )
+        self.framework.observe(
+            self.charm.on[relation_name].relation_changed, self._on_amqp_relation_changed
+        )
+        self.framework.observe(
+            self.charm.on[relation_name].relation_broken, self._on_amqp_relation_broken
+        )
+
+    @property
+    def _amqp_rel(self):
+        return self.framework.model.get_relation(self.relation_name)
+
+    def _on_amqp_relation_joined(self, event):
+        logging.debug("RabbitMQAMQPProvides on_joined")
+        self.on.has_amqp_clients.emit()
+
+    def _on_amqp_relation_changed(self, event):
+        logging.debug("RabbitMQAMQPProvides on_changed")
+        # Validate data on the relation
+        if self.username(event) and self.vhost(event):
+            self.on.ready_amqp_clients.emit()
+
+    def _on_amqp_relation_broken(self, event):
+        logging.debug("RabbitMQAMQPProvides on_departed")
+        # TODO clear data on the relation
+
+    def username(self, event):
+        return event.relation.data[self.charm.app].get("username")
+
+    def vhost(self, event):
+        return event.relation.data[self.charm.app].get("vhost")
+
+    def set_amqp_credentials(self, event, hostname, username, password):
+        logging.debug("Setting amqp connection information")
+        event.relation.data[self.charm.app]["hostname"] = hostname
+        event.relation.data[self.charm.app]["username"] = username
+        event.relation.data[self.charm.app]["password"] = password
+        # Older clients expect the vhost back
+        event.relation.data[self.charm.app]["vhost"] = (
+            self.vhost(event))
+        # TODO TLS Support. The reactive interfaces set ssl_port and ssl_ca
---
+++ src/charm.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# Copyright 2021 David
+# See LICENSE file for licensing details.
+#
+# Learn more at: https://juju.is/docs/sdk
+
+"""Charm the service.
+
+Refer to the following post for a quick-start guide that will help you
+develop a new k8s charm using the Operator Framework:
+
+    https://discourse.charmhub.io/t/4208
+"""
+
+import logging
+
+from charms.interface_rabbitmq_amqp.v0.rabbitmq import RabbitMQAMQPRequires
+from ops.charm import CharmBase
+from ops.framework import StoredState
+from ops.main import main
+from ops.model import ActiveStatus
+
+logger = logging.getLogger(__name__)
+
+
+class PikaOperatorCharm(CharmBase):
+    """Charm the service."""
+
+    _stored = StoredState()
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.framework.observe(self.on.pika_pebble_ready, self._on_pika_pebble_ready)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self._stored.set_default(password=None)
+        # AMQP Requires
+        self.amqp_requires = (RabbitMQAMQPRequires(self, "amqp"))
+        self.framework.observe(self.amqp_requires.on.has_amqp_servers, self._on_has_amqp_servers)
+        self.framework.observe(
+            self.amqp_requires.on.ready_amqp_servers, self._on_ready_amqp_servers)
+        self.framework.observe(self.on.query_amqp_action, self._on_query_amqp)
+
+    def _on_pika_pebble_ready(self, event):
+        # Get a reference the container attribute on the PebbleReadyEvent
+        container = event.workload
+        # Define an initial Pebble layer configuration
+        pebble_layer = {
+            "summary": " layer",
+            "description": "pebble config layer for ",
+            "services": {}
+        }
+        # Add intial Pebble config layer using the Pebble API
+        container.add_layer("", pebble_layer, combine=True)
+        # Autostart any services that were defined with startup: enabled
+        # container.autostart()
+        # Learn more about statuses in the SDK docs:
+        # https://juju.is/docs/sdk/constructs#heading--statuses
+        self.unit.status = ActiveStatus()
+
+    @property
+    def queue(self):
+        return "pika-queue"
+
+    @property
+    def vhost(self):
+        return "pika-vhost"
+
+    @property
+    def username(self):
+        return "pika"
+
+    @property
+    def password(self):
+        return self._stored.password
+
+    def _on_has_amqp_servers(self, event):
+        logging.info("Requesting user and vhost")
+        event.request_access(event, self.username, self.vhost)
+        self.unit.status = ActiveStatus()
+
+    def _on_ready_amqp_servers(self, event):
+        logging.info("Setting password")
+        self._stored.password = event.password
+
+    def _on_config_changed(self, _):
+        pass
+
+    def _on_query_amqp(self, event):
+        import pika
+
+        try:
+            logging.info("Attempting to query AMQP")
+            credentials = credentials = pika.PlainCredentials(self.username, self.password)
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters('localhost', 5672, self.vhost, credentials))
+            channel = connection.channel()
+
+            # Purge queue
+            # http://pika.readthedocs.org/en/latest/modules/channel.html?highlight=purge#pika.channel.Channel.queue_purge
+            channel.queue_declare(queue=self.queue)
+
+            channel.basic_publish(exchange='', routing_key=self.vhost, body='Hello World!')
+            logging.info("Sent 'Hello World!'")
+
+            def callback(ch, method, properties, body):
+                logging.info(" [x] Received %r" % body)
+                event.set_results({"result": "found '{}'".format(body)})
+
+            channel.basic_consume(queue=self.vhost, auto_ack=True, on_message_callback=callback)
+            connection.close()
+        except Exception as e:
+            event.fail(e)
+
+
+if __name__ == "__main__":
+    # Note: use_juju_for_storage=True required per
+    # https://github.com/canonical/operator/issues/506
+    main(PikaOperatorCharm, use_juju_for_storage=True)
